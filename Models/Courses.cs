2741fbdf45823d0bca0774465e64243e56cda024
mryamsh <mryamsh.mohamad@gmail.com>
2026-02-10 10:23:48 +0300

    test it now

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -6,7 +6,7 @@ namespace SchoolApi.Models
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime CourseDatetime { get; set; }
-        public int? LecturerId { get; set; }
+        public int LecturerId { get; set; }
         public Lecturer? Lecturer { get; set; }
         public ICollection<CourseStudent>? CourseStudents { get; set; }
 
--- obj/Debug/net9.0/SchoolApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/SchoolApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820aecf62ea1ac08c1f2109a861fb75607905a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7e1286cb24426ecad199f342d62c3fdf02b220")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
